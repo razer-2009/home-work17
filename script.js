09424dffe2f1976293566f6dc01ed2be5acf1d58
Nikita Nikishow <92978802+razer-2009@users.noreply.github.com>
2022-01-14 17:16:18 +0200

    home-work17
    home-work17

---
+++ script.js
@@ -0,0 +1,35 @@
+var number = prompt("веддите что-то там например 4", 0);
+number = parseInt (number) ;
+if (number = 4){
+    alert("твой счет 4");
+}
+if (number = 555){
+    alert("твой счет 8");
+}
+if (number = 100){
+    alert("error");
+}
+if (number = 1000){
+    alert("error");
+}
+if (number = 200){
+    alert("error");
+}
+if (number = 300){
+    alert("error");
+}
+if (number = 400){
+    alert("error");
+}
+if (number = 500){
+    alert("error");
+}
+if (number = 10000){
+    alert("error");
+}
+if (number = 1){
+    alert("error");
+}
+if (number = 0){
+    alert("error");
+}
